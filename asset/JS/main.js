ca056cd9a45f817c440a4fd8d1b8f2afd33dd94b
FreelancerMamun <mamunkhan16345@gmail.com>
2024-03-28 13:58:44 +0600

    update

--- asset/JS/main.js
+++ asset/JS/main.js
@@ -3,7 +3,33 @@ owl.owlCarousel({
   items: 7,
   loop: true,
   margin: 30,
-  autoplay:true,
+  autoplay: true,
   autoplayTimeout: 2000,
   autoplayHoverPause: true,
+  nav: false,
+});
+
+$('.owl-slider').owlCarousel({
+  loop: true,
+  margin: 30,
+  responsiveClass: true,
+  items: 2,
+  autoplay: false,
+  autoplayTimeout: 3000,
+  nav: false,
+  responsive: {
+    0: {
+      items: 1,
+      nav: false,
+    },
+    600: {
+      items: 2,
+      nav: false,
+    },
+    1000: {
+      items: 2,
+      nav: false,
+      loop: false,
+    },
+  },
 });
